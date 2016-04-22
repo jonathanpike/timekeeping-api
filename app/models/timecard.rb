c7d262e0faec819c288601b41c89fec035c03983
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-22 18:15:12 +0000

    Fix error in calculating worked hours

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -11,7 +11,7 @@ class Timecard < ActiveRecord::Base
     
   def calculate_worked_hours
     return if exception
-    time = ((TimeEntry.last.time - TimeEntry.first.time) / 60 / 60)
+    time = ((TimeEntry.last.time - TimeEntry.first.time) * 24)
     update_attribute(:total_worked_hours, time)
   end 
 end
