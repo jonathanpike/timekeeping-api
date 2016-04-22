26138ec74c657dc05e5b35083f721c0c2cf081d4
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-22 14:10:38 +0000

    Add methods to timecards controller, tests

--- app/controllers/api/v1/timecards_controller.rb
+++ app/controllers/api/v1/timecards_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::TimecardsController < ApplicationController
+  def index
+    cards = Timecard.all
+    render json: { total: cards.count,
+			             timecards: cards	}
+  end 
+  
+  def create
+    card = Timecard.create(timecard_params)
+    if card.valid?
+      render json: card, status: :created
+    else 	
+      render json: card.errors, 
+			       status: :unprocessable_entity
+    end 
+  end 
+  
+  def update
+    card = Timecard.find(params[:id])
+    if card.update_attributes(timecard_params)
+      render json: card, status: :ok
+    else 
+      render json: card.errors, 
+			       status: :unprocessable_entity
+    end 
+  end 
+  
+  def destroy
+    card = Timecard.find(params[:id])
+    if card.destroy
+      render json: {}, status: :ok
+    else 
+      render json: {error: "Timecard could not be deleted."}, status: :unprocessable_entity
+    end 
+  end 
+  
+  private 
+  
+  def timecard_params
+    params.require(:timecard).permit(:occurrence, :username)
+  end 
 end
--- test/controllers/timecards_controller_test.rb
+++ test/controllers/timecards_controller_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class TimecardsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @controller = Api::V1::TimecardsController.new
+    @card = timecards(:one)
+  end 
+  
+  test "should get index" do 
+    get :index
+    assert_response :success
+  end
+  
+  test "should be able to create" do 
+    assert_difference 'Timecard.count' do 
+      post :create, timecard: { occurrence: Date.today, 
+                                username: "Bob" }
+    end 
+    assert_response :created
+  end
+  
+  test "create should fail with invalid params" do 
+    assert_no_difference 'Timecard.count' do 
+      post :create, timecard: { username: "Bobby" } 
+    end 
+    assert_response :unprocessable_entity
+  end
+  
+  test "should be able to update" do 
+    assert @card.username == "Cooper"
+    put :update, id: @card.id, timecard: { username: "Romilly" }
+    assert_response :ok
+    assert @card.reload.username == "Romilly"
+  end
+
+  test "should be able to destroy" do 
+    assert_difference 'Timecard.count', -1 do 
+      delete :destroy, id: @card.id
+    end 
+    assert_response :ok
+  end 
 end
