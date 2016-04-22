ec6fd253e6f6f20753634ed7347c0439e4f51d93
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-04-22 14:10:18 +0000

    Add methods to time entries controller, tests

--- app/controllers/api/v1/time_entries_controller.rb
+++ app/controllers/api/v1/time_entries_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::TimeEntriesController < ApplicationController
+  def index
+    entries = TimeEntry.all
+    render json: { total: entries.count,
+			             timeentries: entries	}
+  end 
+  
+  def create 
+    entry = TimeEntry.create(timeentry_params)
+    if entry.valid?
+      card = Timecard.find(entry.timecard_id)
+      card.exception? 
+      card.calculate_worked_hours if card.exception == false 
+      render json: entry, status: :created
+    else 	
+      render json: entry.errors, 
+			       status: :unprocessable_entity
+    end 
+  end 
+  
+  def update
+    entry = TimeEntry.find(params[:id])
+    if entry.update_attributes(timeentry_params)
+      card = Timecard.find(entry.timecard_id)
+      card.exception? 
+      card.calculate_worked_hours if card.exception == false 
+      render json: entry, status: :ok
+    else 
+      render json: entry.errors, 
+			       status: :unprocessable_entity
+    end
+  end 
+  
+  def destroy
+    entry = TimeEntry.find(params[:id])
+    if entry.destroy
+      render json: {}, status: :ok
+    else 
+      render json: {error: "Time Entry could not be deleted."}, status: :unprocessable_entity
+    end 
+  end 
+  
+  private 
+  
+  def timeentry_params
+    params.require(:time_entry).permit(:time, :timecard_id)
+  end 
 end
--- test/controllers/time_entries_controller_test.rb
+++ test/controllers/time_entries_controller_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class TimeEntriesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    @controller = Api::V1::TimeEntriesController.new
+    @card1 = timecards(:one)
+    @card2 = timecards(:two)
+    @entry = time_entries(:one)
+  end 
+  
+  test "should get index" do 
+    get :index
+    assert_response :success
+  end
+  
+  test "should be able to create" do 
+    assert_difference 'TimeEntry.count' do 
+      post :create, time_entry: { time: DateTime.now, 
+                                  timecard_id: @card2.id }
+    end 
+    assert_not_nil @card2.reload.total_worked_hours
+    assert @card2.reload.exception == false
+    assert_response :created
+  end
+  
+  test "create should fail with invalid params" do 
+    assert_no_difference 'TimeEntry.count' do 
+      post :create, time_entry: { timecard_id: @card2.id } 
+    end 
+    assert_response :unprocessable_entity
+  end
+  
+  test "should be able to update" do 
+    orig_total_worked = @card1.total_worked_hours
+    assert @entry.time == DateTime.new(2001, 2, 3, 4, 5, 6, '+7')
+    put :update, id: @entry.id, time_entry: { time: DateTime.new(2001, 2, 3, 5, 6, 7, '+7') }
+    assert_response :ok
+    assert @entry.reload.time == DateTime.new(2001, 2, 3, 5, 6, 7, '+7')
+    assert_not_equal orig_total_worked, @card1.reload.total_worked_hours
+  end
+
+  test "should be able to destroy" do 
+    assert_difference 'TimeEntry.count', -1 do 
+      delete :destroy, id: @entry.id
+    end 
+    assert_response :ok
+  end 
 end
